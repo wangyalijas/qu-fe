96f85f24a0157b17c3c6f1cbba981c61c36f6e8a
wangyalijas <1114920411@qq.com>
2018-11-30 16:11:08 +0800

    first commit

---
+++ src/views/Hobby.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "Hobby"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/questionnaire.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "questionnaire"
+  }
+</script>
+
+<style scoped>
+
+</style>
